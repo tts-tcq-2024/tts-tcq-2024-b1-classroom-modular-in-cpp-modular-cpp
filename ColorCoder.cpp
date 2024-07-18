73714cc73efa16114dcb0d5d32c2498daced6bcd
Deepa-Dhanashekar <88785401+Deepa-Dhanashekar@users.noreply.github.com>
2024-07-18 12:43:26 +0530

    Update and rename main.cpp to ColorCoder.cpp

---
+++ ColorCoder.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <iomanip>
+#include "ColorCoder.h"
+
+namespace TelCoColorCoder 
+{
+    const char* MajorColorNames[] = {
+        "White", "Red", "Black", "Yellow", "Violet"
+    };
+    const int numberOfMajorColors =
+        sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+    const char* MinorColorNames[] = {
+        "Blue", "Orange", "Green", "Brown", "Slate"
+    };
+    const int numberOfMinorColors =
+        sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+
+    ColorPair::ColorPair(MajorColor major, MinorColor minor):
+        majorColor(major), minorColor(minor) {}
+
+    MajorColor ColorPair::getMajor() const {
+        return majorColor;
+    }
+
+    MinorColor ColorPair::getMinor() const {
+        return minorColor;
+    }
+
+    std::string ColorPair::ToString() const {
+        std::string colorPairStr = MajorColorNames[majorColor];
+        colorPairStr += " ";
+        colorPairStr += MinorColorNames[minorColor];
+        return colorPairStr;
+    }
+
+    ColorPair GetColorFromPairNumber(int pairNumber) {
+        int zeroBasedPairNumber = pairNumber - 1;
+        MajorColor majorColor = 
+            (MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
+        MinorColor minorColor =
+            (MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
+        return ColorPair(majorColor, minorColor);
+    }
+
+    int GetPairNumberFromColor(MajorColor major, MinorColor minor) {
+        return major * numberOfMinorColors + minor + 1;
+    }
+
+    void PrintColorCodingReference() {
+        std::cout << std::left;
+        std::cout << std::setw(15) << "Pair Number" 
+                  << std::setw(15) << "Major Color" 
+                  << "Minor Color" << std::endl;
+
+        for (int pairNumber = 1; pairNumber <= numberOfMajorColors * numberOfMinorColors; ++pairNumber) {
+            ColorPair colorPair = GetColorFromPairNumber(pairNumber);
+            std::cout << std::setw(15) << pairNumber
+                      << std::setw(15) << MajorColorNames[colorPair.getMajor()]
+                      << MinorColorNames[colorPair.getMinor()] << std::endl;
+        }
+    }
+}
